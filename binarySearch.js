6e6f8ae0221f15dd4045dd6b5a9ae5f294c38247
dengtao <dtqwe@qq.com>
2018-07-04 09:30:15 +0800

    二分查找法

---
+++ binarySearch.js
@@ -0,0 +1,20 @@
+function binarySearch(arr, num) {
+  const length = arr.length;
+  let left = 0;
+  let right = length - 1;
+  while(left <= right) {
+    let mid = Math.floor((left+right)/2);
+    if(arr[mid] === num) {
+      console.log(`the index of ${num} is ${mid}`);
+      return mid;
+    } else if(arr[mid] < num) {
+      left = mid+1;
+    } else{
+      right = mid-1;
+    }
+  }
+  console.log(`can not find ${num}`);
+  return false;
+}
+let arr1 = [1,4,6,9,10,15,16,17,18];
+binarySearch(arr1, 6);
