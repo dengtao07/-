610fa7b3eaa9fe2d9f95bb6d610db8f6a1dbf125
dengtao <dtqwe@qq.com>
2018-07-04 10:52:59 +0800

    二分查找

--- binarySearch.js
+++ binarySearch.js
@@ -1,8 +1,8 @@
 function binarySearch(arr, num) {
   const length = arr.length;
   let left = 0;
-  let right = length - 1;  // 在[l...r]的前闭后闭的区间查找
-  while(left <= right) {   // 当l===r时，只有一个元素，认识一个有效数组   
+  let right = length - 1;  //  在[l...r]的前闭后闭的区间查找
+  while(left <= right) {   //  当l===r时，只有一个元素，认识一个有效数组   
     let mid = Math.floor((left+right)/2);
     if(arr[mid] === num) {
       console.log(`the index of ${num} is ${mid}`);
